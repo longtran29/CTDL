1ae79180b2564873660f3bdc02f96a83aad88f8b
tunna29 <tran50787>
2022-07-03 19:48:51 +0700

    constraint letter , number

--- ConstraintTP.h
+++ ConstraintTP.h
@@ -25,7 +25,7 @@ void ConstraintLetterAndNumber(string &chuoi, int order, int width) {
 						}
 						
 						else if(signal == ESC){
-							quit = true;
+							return ;
 							
 						
 							}
@@ -47,3 +47,101 @@ void ConstraintLetterAndNumber(string &chuoi, int order, int width) {
 
 }
 }
+
+
+void ConstraintLetter(string &chuoi, int order, int width) {
+	int lenght = (int)chuoi.length();
+//	gotoxy(x_add + width,order * 3 + y_add);
+	gotoxy(x_add+width,order*3+y_add);
+	cout << chuoi;
+	int count = lenght;
+
+	bool quit = false;
+	while(!quit)
+	{
+			int signal = _getch();
+			if(signal != 224 && signal != BACKSPACE && signal !=0) {
+			
+						if( ( 65 <= signal && signal <= 90 ) || ( 97 <= signal && signal <= 122 ) )
+						{
+							
+							if( count < 15 )
+							{
+								count++; 
+								cout << (char)signal;// hien thi
+								chuoi += (char)signal;// cap nhat 
+							}
+						}
+						
+						else if(signal == ESC){
+							return ;
+							
+						
+							}
+						else if(signal == ENTER) {
+							return;
+						}
+					
+					}
+					
+	
+			else if(signal == BACKSPACE && count >0) {
+				putch('\b');
+				putch(' ');
+				putch('\b');
+				chuoi.erase(chuoi.length()-1, 1);
+				count--;
+			}
+	
+
+}
+}
+
+
+void ConstraintNumber(int &num, int order, int width, int limitseats) {
+	gotoxy(x_add+width,order*3+y_add);
+	
+	bool quit = false;
+	while(!quit)
+	{
+			int signal = _getch();
+			if(signal != 224 && signal != BACKSPACE && signal !=0) {
+			
+						if(  48 <= signal && signal <= 57 )
+						{
+							
+							int result = signal - 48;
+							if(signal == 48) {
+								continue;	
+							}
+							else if((num*10+ result) < limitseats ) {
+								
+								num = num * 10 +result;
+								cout<<result;
+							
+							}
+						}
+						
+						else if(signal == ESC){
+							return ;
+							
+						
+							}
+						else if(signal == ENTER) {
+							return;
+						}
+					
+			}
+			else if(signal == BACKSPACE  ) {
+				cout << (char)BACKSPACE<<" "<<(char)BACKSPACE;
+				num /=10;
+			}
+					
+	
+}
+	
+	
+	
+}
+
+
