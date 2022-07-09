ee57f6aa3bd0d67ab73457c1fbb3eade7a11ce0e
tunna29 <tran50787>
2022-07-10 01:16:48 +0700

    up flight.h

--- Flight.h
+++ Flight.h
@@ -2,7 +2,7 @@
 
 #include "Ticket.h"
 
-string ContentFlight[]= { "Ma Chuyen Bay","San Bay Den"	,"So Hieu May Bay","Thoi Gian Di","Tong So Ve","Trang Thai"};
+string ContentFlight[]= { "Ma Chuyen Bay","San Bay Den"	,"So Hieu May Bay","Tong So Ve","Trang Thai"};
 
 struct Flight {
 
@@ -13,7 +13,7 @@ struct Flight {
 	char serialPlane[15];
 	int totalTicket;
 	int saleTotal;
-	Ticket *TicketList;
+//	Ticket *TicketList;
 
 };
 
@@ -21,17 +21,37 @@ struct Flight {
 struct flightNode {
 	
 	Flight flight;
-	Flight *pNext;
+	flightNode *pNext;
 };
 
-typedef  flightNode * PTR;
+typedef  flightNode* PTR;
 
-PTR First=NULL;
 
-void Nhap_Chuyen_Bay(flightNode list) {
+
+
+void addToListPlane(PTR &first, Flight &cb ) {
+	
+	PTR p;
+   	p = new flightNode;
+
+
+	if(first == NULL) {
+		p->flight = cb;
+		p->pNext = first;
+		first= p;
+	}
+	
+	
+	
+}
+
+void Nhap_Chuyen_Bay(PTR &p) {
 
 	string ID;
 	string destination;
+	string serialPlane;
+	int nTicket = 0 ;
+	int status = 0 ;
 	int order = 0;
 	
 	while(true) {
@@ -55,8 +75,46 @@ void Nhap_Chuyen_Bay(flightNode list) {
 				order++;
 				break;	
 			case 2:
+				ConstraintLetterAndNumber(serialPlane,order,17);
+				if(serialPlane == "") {
+					
+					BaoLoi(" Vui Long Khong Bo Trong ");
+					break;
+				}
+				order++;
+				break;	
+			case 3:
+				ConstraintForOnlyNumber(nTicket,order,17,999);
+				if(nTicket<20) {
+					BaoLoi(" Khong hop le ");
+					break;
+				}
+				order++;
 				break;
-			
+			case 4:
+				gotoxy(X_Notification,Y_Notification+1);cout <<"1 = Huy     ";
+				gotoxy(X_Notification,Y_Notification+2);cout <<"2 = Con ve  ";
+				gotoxy(X_Notification,Y_Notification+3);cout <<"3 = Het ve  ";
+				gotoxy(X_Notification,Y_Notification+4);cout <<"4 = Hoan tat";
+				ConstraintForOnlyNumber(status,order,17, 4);
+				order++;
+				break;
+			case 5:
+				{
+					Flight flight;
+					strcpy(flight.flightCode, ID.c_str());
+					strcpy(flight.arrivalPlace, destination.c_str());
+					strcpy(flight.serialPlane, serialPlane.c_str());
+					flight.totalTicket = nTicket;
+					flight.status = status;
+					flight.saleTotal = 0;
+					addToListPlane(p, flight);
+					
+					
+					
+						
+				}
+				break;
 			
 		}
 		
@@ -67,7 +125,7 @@ void Nhap_Chuyen_Bay(flightNode list) {
 
 }
 
-void ManageFlightPlane(flightNode planeList) {
+void ManageFlightPlane(PTR &p) {
 	
 	system("cls");
 	
@@ -75,7 +133,7 @@ void ManageFlightPlane(flightNode planeList) {
 
 	cout << " Quan ly chuyen bay ";
 	
-	Display(ContentFlight,6);
+	Display(ContentFlight,5);
 	int signal;
 	
 
@@ -91,9 +149,9 @@ void ManageFlightPlane(flightNode planeList) {
 				
 				if(signal == INSERT) {
 					system("cls");
-					CreateForm(ContentFlight,3,27);
+					CreateForm(ContentFlight,5,27);
 					gotoxy(115 + 12,0 * 3 + 4);
-					Nhap_Chuyen_Bay(planeList);
+					Nhap_Chuyen_Bay(p);
 										
 										
 				}
