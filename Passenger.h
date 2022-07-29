3192438f6e4fe9f5cdd6c04babcb37d26e64434c
tunna29 <tran50787>
2022-07-30 00:04:46 +0700

    update form input passenger

--- Passenger.h
+++ Passenger.h
@@ -11,14 +11,17 @@ typedef struct passenger Passenger;
 
 struct passengernode {
 	passenger data;
-	struct passenger* pLeft = NULL;
-	struct passenger* pRight = NULL;
+	struct passengernode* pLeft = NULL;
+	struct passengernode* pRight = NULL;
 	int bf;
 };
 typedef passengernode PassengerNode;
 typedef passengernode* AVLTree;
 
 AVLTree root;
+
+string ContentPassenger[5] = {"STT", "CMND", "Ho", "Ten", "GioiTinh"};
+
 
 void CreateAVLTree(AVLTree &root) {
 	root = NULL;
@@ -104,6 +107,7 @@ void ChangeTicket(int pos) {
 
 
 int ChooseTicket(Flight F) {
+	ShowCur(false);
 	CurPosTicket = 1;
 	CurPosPreTicket = 1;
 	
@@ -147,28 +151,230 @@ int ChooseTicket(Flight F) {
 				break;
 			case ESC:
 				return -1;
+			case ENTER:
+				if(CheckStatus(F,CurPosTicket) == 1) {
+					BaoLoi("Ghe nay da co ng dat");
+					break;
+				}
+				quit = true;
+				return CurPosTicket;
+				
 				
 		}
 	}
 	
 	
-
-	
-	
-
 }
 
+AVLTree CreateTree(Passenger data) {
+	
+	AVLTree newPsg = new PassengerNode;
+	newPsg->data = data;
+	newPsg->pLeft = NULL;
+	newPsg->pRight = NULL;
+	return newPsg;
+}
+
+
+int height(AVLTree r) {
+	
+	if(r == NULL) return -1;
+	
+	else {
+		
+		int lheight = height(r->pLeft);
+		int rheight = height(r->pRight);
+		
+		if(lheight> rheight) return lheight+1;
+		else return rheight+1;
+	}
+}
+
+
+int getBalanceFactor(AVLTree n) {
+	if(n == NULL) return -1;
+	return height(n->pLeft) - height(n->pRight);
+}
+
+AVLTree rightRotate(AVLTree y) {
+	
+	AVLTree x = y->pLeft;
+	AVLTree T2 = x->pRight;
+	
+	// perfrom r
+	x->pRight = y;
+	y->pLeft  = T2;
+	return x;
+	
+	
+}
+
+
+AVLTree leftRotate(AVLTree x) {
+	
+	AVLTree y = x->pRight;
+	AVLTree T2 = y->pLeft;
+	
+	// perform rotate
+	y->pLeft = x;
+	x->pRight = T2;
+	
+	return y;
+}
+
+PassengerNode* findPassenger(AVLTree root, int CMND) {
+	
+	if(root == NULL) return NULL;
+	 else if(root->data.CMND == CMND) return root;
+	
+	else if(root->data.CMND > CMND) return findPassenger(root->pLeft, CMND);
+	else return findPassenger(root->pRight, CMND);
+	
+}
+
+AVLTree AddPassenger(AVLTree root, AVLTree newPsg) {
+	
+	if(root == NULL) {
+		root = newPsg;
+		BaoLoi("Inserted !");
+		return root;
+	}
+	
+	else if(newPsg->data.CMND < root->data.CMND) root->pLeft =  AddPassenger(root->pLeft, newPsg);
+	else if(newPsg->data.CMND > root->data.CMND) root->pRight  = AddPassenger(root->pRight, newPsg);
+	
+	else return root;
+	
+	int bf = getBalanceFactor(root);
+	
+	
+	// left left case
+	if(bf> 1  && newPsg->data.CMND < root->pLeft->data.CMND) return rightRotate(root);
+	
+	// right right case
+	if(bf <-1 && newPsg->data.CMND > root->pRight->data.CMND) return leftRotate(root);
+	//left right case
+	if(bf>1 && newPsg->data.CMND > root->pLeft->data.CMND) {
+		root->pLeft = leftRotate(root->pLeft);
+		return rightRotate(root);
+	}
+	
+	// right left case
+	if(bf<-1 && newPsg->data.CMND < root->pRight->data.CMND) {
+		root->pRight = rightRotate(root->pRight);
+		return leftRotate(root);
+	}
+	
+	// giu nguyen nut goc 
+	return root;
+	
+	
+	
+}
+
+
+
+void CreatePassenger(AVLTree root, bool Edit, bool Del, int ID) {
+	bool Save = true;
+	
+	int order = 1;
+	string firstName;
+	string lastName;
+	int gender = 1; // 0 : female , 1: male
+	
+	
+	gotoxy(X_Notification, Y_Notification); cout<<" 0 la nu , 1 la nam";
+	
+	int ch ;
+	
+	while(true) {
+		
+		switch(order) {
+			
+			case 1 : // input ho
+				{
+					ConstraintsForLetterAndSpace(firstName,Save,order,12);
+					if(Save == false) {
+						return ;
+					}
+					
+					if(firstName == "")	{
+						BaoLoi("Khong bo trong");
+					} else order++; 					 
+					
+				}
+				
+					break;
+			case 2: // ten
+				{
+					ConstraintLetter(lastName,order,Save, 12);
+					if(Save == false) return;
+					
+					if(lastName ==  "") BaoLoi("Khong bo trong");
+					
+					order++;
+					break;
+				
+					
+				}
+			case 3: // gioi tinh
+				{
+					ConstraintForOnlyGender(gender,order,Save,12);
+			
+					if(Save == false ) return;
+					if(gender == -1) BaoLoi("Khong bo trong");
+					
+					order++;
+				
+				}
+				break;
+			case 4:
+				{
+					Passenger psg;
+					psg.CMND = ID;
+					strcpy(psg.Surname,firstName.c_str());
+					strcpy(psg.Name,lastName.c_str());
+					psg.Gender = gender;
+					
+					AVLTree newPsg =  CreateTree(psg);
+					
+					AddPassenger(root,newPsg);
+					BaoLoi("Add sucess !");
+					RemoveFormComplete();				
+					return;
+					
+				}
+				break;
+			
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
+
+
+
 void BookTicket(AVLTree root) {
+
 	system("cls");
 	system("color 0E");
 	gotoxy(X_Title, Y_Title);
-	cout << "Vui long nhap ma chuyen bay  --->";
+	cout << "Vui long nhap ma chuyen bay : ";
 	
 	bool flag = false;
 	int order = 0;
 	bool Save = true;
 	string result;
 	PTR_FL currentFlight = NULL; 
+	
 	
 	while(flag == false ) {
 		ConstraintLetterAndNumber(result,order,Save, 12);
@@ -193,20 +399,78 @@ void BookTicket(AVLTree root) {
 	
 }
 		system("cls");
-		gotoxy(X_Title, Y_Title);
-		cout << " Danh sach hanh khach co ma chuyen bay " << currentFlight->flight.flightCode << " toi " << currentFlight->flight.arrivalPlace<< " luc ";
-		OutputDateTime(currentFlight->flight.departTime);
 		
-		gotoxy(20,5);
-		cout << "Warn: VE MAU DO LA DA CO NGUOI DAT ROI !!";
 		
 		while(1) {
 			
+			gotoxy(X_Title, Y_Title);
+			cout << " Danh sach hanh khach co ma chuyen bay " << currentFlight->flight.flightCode << " toi " << currentFlight->flight.arrivalPlace<< " luc ";
+			OutputDateTime(currentFlight->flight.departTime);
+			
+			gotoxy(20,5);
+			cout << "Warn: VE MAU DO LA DA CO NGUOI DAT ROI !!";
+			
+			system("color 0E");
 			int choosen = ChooseTicket(currentFlight->flight);
+			
+			if(choosen == -1) return;
+			ShowCur(true);
+			
+			gotoxy(X_Title,Y_Title);
+			
+			system("cls");
+			
+			
+			system("color 0E");
+			cout<<"Nhap CMND :";
+			int IDHanhKhach = 0; int target = -1;
+			
+			CreateForm(ContentPassenger,1,5,30);			
 		
+			ConstraintForOnlyNumber(IDHanhKhach, order, Save, 12, 999999);
+			
+			if(Save == false) continue;
+			
+			for(int i=0; i< currentFlight->flight.saleTotal; i++) {
+				
+				if(currentFlight->flight.TicketList[i].CMND == IDHanhKhach) { // tim trong danh sach ve ? co ton tai hanh khach cmnd ?
+					target = i;
+					break;
+				}
+			
+			}
+			
+			if(target > -1) {
+				
+				BaoLoi("Ban da dat ve roi");
+				RemoveFormComplete();
+				continue;
+			
+			}
+			
+			if(target == -1) { // khong co trong listTicket
+				
+				PassengerNode* findedPassenger = findPassenger(root,IDHanhKhach);
+				
+				BaoLoi("Khong tim thay !");
+				
+				if(findedPassenger == NULL) {
+					CreateForm(ContentPassenger, 1, 5, 27);
+					CreatePassenger(root,false,false,IDHanhKhach);
+			
+				}
+			
+				Ticket newTicket;
+				newTicket.CMND = IDHanhKhach;
+				newTicket.Status = choosen;
+				
+				currentFlight->flight.TicketList[currentFlight->flight.saleTotal] = newTicket ;
+				currentFlight->flight.saleTotal++;
+			}
 		
 		}	
 
+		
 
 
 }
